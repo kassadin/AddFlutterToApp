c33ac8ee5425afd63e2eceb8e9039bbf461667f2
kassadin <kassadin@foxmail.com>
2019-09-25 01:32:45 +0800

    osx ok ？？

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Add Flutter to App'),
+      home: MyHomePage(title: 'Add Flutter to App on Mac Test'),
     );
   }
 }
